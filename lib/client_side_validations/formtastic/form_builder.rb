ba90584635d536ba95363546fc0c4bdacd15bb46
John Maxwell <john@musicglue.com>
2014-02-25 19:00:17 +0000

    rails 4.1

--- lib/client_side_validations/formtastic/form_builder.rb
+++ lib/client_side_validations/formtastic/form_builder.rb
@@ -1,15 +1,14 @@
 module ClientSideValidations
   module Formtastic
     module FormBuilder
+      extend ActiveSupport::Concern
 
-      def self.included(base)
-        base.class_eval do
-          def client_side_form_settings(options, form_helper)
-            {
-              :type => self.class.to_s,
-              :inline_error_class => ::Formtastic::FormBuilder.default_inline_error_class
-            }
-          end
+      included do#
+        def client_side_form_settings(options, form_helper)
+          {
+            :type => self.class.to_s,
+            :inline_error_class => ::Formtastic::FormBuilder.default_inline_error_class
+          }
         end
       end
 
